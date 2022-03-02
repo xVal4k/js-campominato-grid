e52b25e8789e6d0d661a2c4ddfe8a124c6ae7321
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-02 17:45:41 +0100

    js easy square

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
 let squareArea = document.querySelector(".square-area");
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 1; i <= 49; i++) {
     let square = document.createElement("div");
-    square.classList.add("box");
+    square.classList.add("box_easy");
     square.innerHTML = i;
 
     squareArea.append(square);
