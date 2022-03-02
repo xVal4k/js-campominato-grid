f24cd9799a8ef6b5098d195f497710f6c29749d7
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-02 18:09:51 +0100

    added interaction with play button

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,50 @@
-let squareArea = document.querySelector(".square-area");
+let squareArea = document.querySelector(".square_area");
+const bntPlay = document.querySelector("#play");
+const selectDifficulty = document.getElementById('select_difficulty');
 
-for (let i = 1; i <= 49; i++) {
-    let square = document.createElement("div");
-    square.classList.add("box_easy");
-    square.innerHTML = i;
+bntPlay.addEventListener("click", function() {
 
-    squareArea.append(square);
-}
+    squareArea.innerHTML = " ";
+
+    let difficulty = selectDifficulty.value;
+
+    if (difficulty == "easy") {
+
+        for (let i = 1; i <= 49; i++) {
+            let square = document.createElement("div");
+            square.classList.add("box_easy");
+            square.innerHTML = i;
+        
+            squareArea.append(square);
+        }
+
+    } else if (difficulty == "normal") {
+
+        for (let i = 1; i <= 81; i++) {
+            let square = document.createElement("div");
+            square.classList.add("box_medium");
+            square.innerHTML = i;
+        
+            squareArea.append(square);
+        }
+
+    } else if (difficulty == "hard") {
+
+        for (let i = 1; i <= 100; i++) {
+            let square = document.createElement("div");
+            square.classList.add("box_hard");
+            square.innerHTML = i;
+        
+            squareArea.append(square);
+        }
+
+    }
+});
+
+
+
+
+// ogni volta che clicco play la pagina si pulisce
+// in base alla difficoltà mi appare la griglia
+
+
